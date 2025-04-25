e9e226e06d013a1b6db256941aefc7c2d9b64fc4
JustOrangeDrink <sadcat2005@gmail.com>
2025-04-25 03:23:11 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,68 @@
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+
+const SCREEN_WIDTH = html.clientWidth;
+const SCREEN_HEIGHT = html.clientHeight;
+
+const tileSize = 24;
+
+const canvas = document.querySelector("canvas");
+//Recalculating size so it in tiles boundaries
+canvas.width = Math.floor(SCREEN_WIDTH / tileSize) * tileSize;
+canvas.height = Math.floor(SCREEN_HEIGHT / tileSize) * tileSize;
+
+const ctx = canvas.getContext("2d");
+
+const spritesheet = document.createElement("img");
+spritesheet.src = "assets/spritesheet.png";
+
+spritesheet.onload = renderWorld;
+
+function drawTile([char_x, char_y], color) {
+  const offscreenCanvas = new OffscreenCanvas(tileSize, tileSize);
+  const offscreenCtx = offscreenCanvas.getContext("2d");
+
+  offscreenCtx.fillStyle = color;
+  offscreenCtx.fillRect(0, 0, tileSize, tileSize);
+
+  offscreenCtx.globalCompositeOperation = "destination-in";
+
+  offscreenCtx.drawImage(
+    spritesheet,
+    char_x * tileSize,
+    char_y * tileSize,
+    tileSize,
+    tileSize,
+    0,
+    0,
+    tileSize,
+    tileSize
+  );
+  return offscreenCanvas;
+}
+
+const entities = [];
+class Entity {
+  constructor(x, y, char, color, collision) {
+    this.x = x;
+    this.y = y;
+    this.dx = 0;
+    this.dy = 0;
+    this.char = char;
+    this.color = color;
+    this.collision = collision;
+
+    entities.push(this);
+  }
+}
+
+const zombie = new Entity(11, 15, [10, 5], "green", true);
+const zofembie = new Entity(13, 3, [10, 5], "red", true);
+
+function renderWorld() {
+  for (let i = 0; i < entities.length; i++) {
+    let entity = entities[i];
+    const entityTile = drawTile(entity.char, entity.color);
+    ctx.drawImage(entityTile, entity.x * tileSize, entity.y * tileSize);
+  }
+}
