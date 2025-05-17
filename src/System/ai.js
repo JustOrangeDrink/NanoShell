7e7add799b5846c764b45ac68b75eef458ada3fa
JustOrangeDrink <sadcat2005@gmail.com>
2025-05-17 16:33:32 +0300

    FEATURE: Zombies now walk towards the player as a behavior.

--- src/System/ai.js
+++ src/System/ai.js
@@ -1,17 +1,32 @@
-import { randomInt } from "../utils.js";
+import { getEntity, randomInt } from "../utils.js";
 import { tryMovement } from "./engine.js";
 
+let player;
 function zombieBehavior(entity) {
-  const axisCoin = randomInt(0, 1);
-  const directionCoin = randomInt(0, 1);
+  if (!player) player = getEntity(0, "Player");
 
-  if (axisCoin) {
-    if (directionCoin) tryMovement(entity, 1, 0);
-    else tryMovement(entity, -1, 0);
-  } else {
-    if (directionCoin) tryMovement(entity, 0, 1);
-    else tryMovement(entity, 0, -1);
-  }
+  let dx;
+  let dy;
+  if (player.x === entity.x) dx = 0;
+  if (player.x < entity.x) dx = -1;
+  if (player.x > entity.x) dx = 1;
+
+  if (player.y === entity.y) dy = 0;
+  if (player.y < entity.y) dy = -1;
+  if (player.y > entity.y) dy = 1;
+  tryMovement(entity, dx, dy);
+  return;
+
+  // const axisCoin = randomInt(0, 1);
+  // const directionCoin = randomInt(0, 1);
+
+  // if (axisCoin) {
+  //   if (directionCoin) tryMovement(entity, 1, 0);
+  //   else tryMovement(entity, -1, 0);
+  // } else {
+  //   if (directionCoin) tryMovement(entity, 0, 1);
+  //   else tryMovement(entity, 0, -1);
+  // }
 }
 
 export { zombieBehavior };
