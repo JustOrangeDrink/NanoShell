079992fb630b665ebdd81f1d80e1103861252032
Amir <amirislamov205@gmail.com>
2025-05-29 06:35:39 +0300

    COMMON: Added spacing to items in inventory ui.

--- src/ui/inventory.js
+++ src/ui/inventory.js
@@ -61,8 +61,8 @@ function updateInventoryUi() {
       MENU_Y + 14 + i * 36
     );
     inventoryUiCtx.fillText(
-      `a ${item.name}`,
-      MENU_X + 45,
+      `- a ${item.name}`,
+      MENU_X + 36,
       MENU_Y + 30 + i * 36
     );
   }
