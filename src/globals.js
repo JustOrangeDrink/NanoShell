765b1ee9b045e1dd52d8d18ff3de1d3bc67353d9
Amir <amirislamov205@gmail.com>
2025-05-31 17:01:44 +0300

    COMMON: Canvases are now labeled with classes.

--- src/globals.js
+++ src/globals.js
@@ -10,23 +10,23 @@ const SCREEN_HEIGHT = html.clientHeight;
 const CANVAS_TILED_WIDTH = Math.floor((SCREEN_WIDTH * 0.7) / TILE_SIZE);
 const CANVAS_TILED_HEIGHT = Math.floor(SCREEN_HEIGHT / TILE_SIZE);
 
-const canvas = document.querySelector("canvas");
+const canvas = document.getElementsByClassName("game")[0];
 const ctx = canvas.getContext("2d");
 
-const mainUiCanvas = document.querySelectorAll("canvas")[1];
+const mainUiCanvas = document.getElementsByClassName("main_ui")[0];
 const mainUiCtx = mainUiCanvas.getContext("2d");
 
-const inventoryUiCanvas = document.querySelectorAll("canvas")[2];
+const inventoryUiCanvas = document.getElementsByClassName("inventory_ui")[0];
 const inventoryUiCtx = inventoryUiCanvas.getContext("2d");
-
-inventoryUiCanvas.width = SCREEN_WIDTH;
-inventoryUiCanvas.height = SCREEN_HEIGHT;
 
 canvas.width = CANVAS_TILED_WIDTH * TILE_SIZE;
 canvas.height = CANVAS_TILED_HEIGHT * TILE_SIZE;
 
 mainUiCanvas.width = CANVAS_TILED_WIDTH * 0.5 * TILE_SIZE;
 mainUiCanvas.height = CANVAS_TILED_HEIGHT * TILE_SIZE;
+
+inventoryUiCanvas.width = SCREEN_WIDTH;
+inventoryUiCanvas.height = SCREEN_HEIGHT;
 
 const MAP_TILED_WIDTH = 100;
 const MAP_TILED_HEIGHT = 100;
