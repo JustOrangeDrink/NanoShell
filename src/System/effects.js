12d2647373f23526cbc0fcf9558a027516010efc
Amir <amirislamov205@gmail.com>
2025-06-10 21:09:14 +0300

    FIX: Random room in randomTp effect now doesn't include unexisting "last+1" room.

--- src/System/effects.js
+++ src/System/effects.js
@@ -3,13 +3,17 @@ import { addLog } from "../ui/sidebar.js";
 import { randomInt } from "../utils.js";
 
 function randomTp(trg) {
-  const randomRoom = rooms[randomInt(0, rooms.length)];
+  const randomRoom = rooms[randomInt(0, rooms.length - 1)];
+
   tilemap[trg.y][trg.x].splice(tilemap[trg.y][trg.x].indexOf(trg), 1);
   tilemap[randomRoom.getCenter().y][randomRoom.getCenter().x].push(trg);
+
+  // remove player from his knowledge map (when player tp's he remember himself on this spot)
   for (let i = 0; i < knownMap[trg.y][trg.x].length; i++) {
     const entity = knownMap[trg.y][trg.x][i];
     if (entity.id == trg.id) knownMap[trg.y][trg.x].splice(i, 1);
   }
+
   trg.x = randomRoom.getCenter().x;
   trg.y = randomRoom.getCenter().y;
   addLog(`${trg.name} vanishes!`, "purple");
