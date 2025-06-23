98527551b3e3aa5693d7c1bb68566f2c1a0545cf
Amir <amirislamov205@gmail.com>
2025-06-23 20:31:39 +0300

    FEATURE: Initial colorizing now recolors purple png tilesets as well.

--- src/utils.js
+++ src/utils.js
@@ -51,7 +51,12 @@ function colorize(
   const imageData = ctx.getImageData(0, 0, TILE_SIZE, TILE_SIZE);
 
   for (let i = 0; i < imageData.data.length; i += 4) {
-    if (imageData.data[i + 3] === 0) {
+    if (
+      (imageData.data[i + 0] === 255 &&
+        imageData.data[i + 1] === 0 &&
+        imageData.data[i + 2] === 255) ||
+      imageData.data[i + 3] === 0
+    ) {
       imageData.data[i + 0] = bgR;
       imageData.data[i + 1] = bgG;
       imageData.data[i + 2] = bgB;
